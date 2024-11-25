b2e7e011aa1680a2eb1d65f02c3f2905e6408b35
Danila Kondratenko <dan.kondratenko2013@ya.ru>
2024-11-25 12:07:43 +0600

    Add OpRemoveDirectory

--- WPIW/operations.h
+++ WPIW/operations.h
@@ -7,3 +7,4 @@ HRESULT OpMoveFile(char *Source, char *Destination);
 HRESULT OpCopySingleFile(char *Source, char *Destination);
 HRESULT OpCopyDirectory(char *Source, char *Destination);
 HRESULT OpRemoveFile(char *File);
+HRESULT OpRemoveDirectory(char *Directory);
--- WPIW/recurse.c
+++ WPIW/recurse.c
@@ -1,19 +1,49 @@
 #include "operations.h"
 #include "utils.h"
+#include "tests.h"
 
 #include <string.h>
 #include <wchar.h>
 #include "sds.h"
 #include "cwalk.h"
 
-static HRESULT copy_recursively(LPWSTR Source, LPWSTR Destination);
+typedef HRESULT (*file_callback)(LPWSTR arg1, LPWSTR arg2);
+
+static HRESULT copy_single(LPWSTR Source, LPWSTR Destination)
+{
+	CopyFileW(Source, Destination, FALSE);
+	return HRESULT_FROM_WIN32(GetLastError());
+}
+
+static HRESULT delete_single(LPWSTR File, LPWSTR Unused)
+{
+	(void)Unused;
+	DeleteFileW(File);
+	return HRESULT_FROM_WIN32(GetLastError());
+}
+
+static HRESULT create_destination(LPWSTR Unused, LPWSTR Destination)
+{
+	(void)Unused;
+	CreateDirectoryW(Destination, NULL);
+	return HRESULT_FROM_WIN32(GetLastError());
+}
+
+static HRESULT delete_folder(LPWSTR Source, LPWSTR Unused)
+{
+	(void)Unused;
+	RemoveDirectoryW(Source);
+	return HRESULT_FROM_WIN32(GetLastError());
+}
+
+static HRESULT enum_recursively(LPWSTR Source, LPWSTR Destination, file_callback begin_cb, file_callback cb, file_callback end_cb);
 
 HRESULT OpCopyDirectory(char *Source, char *Destination)
 {
 	HRESULT result = S_OK;
 	LPWSTR szwSource, szwDestination;
 	char *BaseName;
-
+	
 	szwSource = UTF8ToWideCharAlloc(Source);
 	if (!szwSource) {
 		result = E_OUTOFMEMORY;
@@ -41,11 +71,29 @@ HRESULT OpCopyDirectory(char *Source, char *Destination)
 		goto cleanup1;
 	}
 
-	result = copy_recursively(szwSource, szwDestination);
+	result = enum_recursively(szwSource, szwDestination, create_destination, copy_single, NULL);
 
 cleanup2:
 	HeapFree(GetProcessHeap(), 0, szwDestination);
 cleanup1:
+	HeapFree(GetProcessHeap(), 0, szwSource);
+exit_fn:
+	return result;
+}
+
+HRESULT OpRemoveDirectory(char* Source)
+{
+	HRESULT result = S_OK;
+	LPWSTR szwSource;
+	char* BaseName;
+
+	szwSource = UTF8ToWideCharAlloc(Source);
+	if (!szwSource) {
+		result = E_OUTOFMEMORY;
+		goto exit_fn;
+	}
+
+	result = enum_recursively(szwSource, NULL, NULL, copy_single, delete_folder);
 	HeapFree(GetProcessHeap(), 0, szwSource);
 exit_fn:
 	return result;
@@ -74,23 +122,24 @@ static LPWSTR append_alloc(LPWSTR Path1, LPWSTR Path2)
 	return NewPath;
 }
 
-static HRESULT copy_recursively(LPWSTR Source, LPWSTR Destination)
+static HRESULT enum_recursively(LPWSTR Source, LPWSTR Destination, file_callback begin_cb, file_callback cb, file_callback end_cb)
 {
-	size_t SourceLength, DestinationLength;
 	WIN32_FIND_DATAW FindData = { 0 };
 	HANDLE hFind;
 	HRESULT result = S_OK;
 
-	if (!CreateDirectoryW(Destination, NULL)) {
-		return HRESULT_FROM_WIN32(GetLastError());
+	if (begin_cb) {
+		result = begin_cb(Source, Destination);
+		if (FAILED(result))
+			return result;
 	}
 
-	SourceLength = wcslen(Source);
-	DestinationLength = wcslen(Destination);
-
 	hFind = FindFirstFileW(Source, &FindData);
+	if (INVALID_HANDLE_VALUE == hFind) {
+		return HRESULT_FROM_WIN32(GetLastError());
+	}
 	do {
-		LPWSTR NewSource, NewDestination;
+		LPWSTR NewSource, NewDestination = NULL;
 
 		if (FindData.cFileName[0] == '.' && !FindData.cFileName[1])
 			continue;
@@ -103,26 +152,31 @@ static HRESULT copy_recursively(LPWSTR Source, LPWSTR Destination)
 			goto cleanup1;
 		}
 
-		NewDestination = append_alloc(Destination, FindData.cFileName);
-		if (!NewDestination) {
-			result = E_OUTOFMEMORY;
-			goto cleanup2;
+		if (Destination) {
+			NewDestination = append_alloc(Destination, FindData.cFileName);
+			if (!NewDestination) {
+				result = E_OUTOFMEMORY;
+				goto cleanup2;
+			}
 		}
 
 		if (FindData.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY) {
-			result = copy_recursively(NewSource, NewDestination);
+			result = enum_recursively(NewSource, NewDestination, begin_cb, cb, end_cb);
 		}
 		else {
-			CopyFileW(NewSource, NewDestination, FALSE);
-			result = HRESULT_FROM_WIN32(GetLastError());
+			result = cb(NewSource, NewDestination);
 		}
 
 	cleanup3:
-		HeapFree(GetProcessHeap(), 0, NewDestination);
+		if (NewDestination) HeapFree(GetProcessHeap(), 0, NewDestination);
 	cleanup2:
 		HeapFree(GetProcessHeap(), 0, NewSource);
 	}
 	while (SUCCEEDED(result) && FindNextFileW(hFind, &FindData));
+
+	if (end_cb) {
+		result = end_cb(Source, Destination);
+	}
 
 cleanup1:
 	FindClose(hFind);
