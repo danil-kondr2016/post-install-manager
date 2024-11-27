3fa60037745adc98e0ff0b6fe5a2b45f38bee4bf
Danila Kondratenko <danilakondratenko95@gmail.com>
2024-11-27 09:26:03 +0000

    Add WideCharToSdsAlloc

--- WPIW/utils.c
+++ WPIW/utils.c
@@ -1,4 +1,5 @@
 #include "utils.h"
+#include "sds.h"
 
 #include <windows.h>
 
@@ -14,6 +15,21 @@ wchar_t *UTF8ToWideCharAlloc(char *source)
 		return NULL;
 	MultiByteToWideChar(CP_UTF8, 0, source, -1, result, resultSize);
 	result[resultSize] = 0;
+
+	return result;
+}
+
+sds WideCharToSdsAlloc(wchar_t *source)
+{
+	DWORD resultSize;
+	sds result;
+
+	resultSize = WideCharToMultiByte(CP_UTF8, 0, source, -1, NULL, 0, NULL, NULL);
+	result = sdsgrowzero(sdsempty(), resultSize + 1);
+	if (!result)
+		return NULL;
+	WideCharToMultiByte(CP_UTF8, 0, source, -1, result, resultSize, NULL, NULL);
+	sdsupdatelen(result);
 
 	return result;
 }
--- WPIW/utils.h
+++ WPIW/utils.h
@@ -1,5 +1,7 @@
 #pragma once
 
 #include <wchar.h>
+#include "sds.h"
 
 wchar_t *UTF8ToWideCharAlloc(char *source);
+sds WideCharToSdsAlloc(wchar_t *source);
