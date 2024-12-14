ac3d50dd5c025d777f55704670a6a6dc836593ea
Danila Kondratenko <dan.kondratenko2013@ya.ru>
2024-12-14 06:31:10 +0000

    Add arena allocator functions

---
+++ src/postinst/arena.c
@@ -0,0 +1,54 @@
+#include "arena.h"
+#include "fatal.h"
+
+#include <windows.h>
+
+struct arena arena_create(ptrdiff_t size)
+{
+	struct arena result = {0};
+
+	result.base = VirtualAlloc(NULL, size, MEM_RESERVE|MEM_COMMIT, PAGE_READWRITE);
+	if (!result.base)
+		fatal_errorW(L"Out of memory", STATUS_NO_MEMORY);
+
+	result.begin = result.base;
+	result.end = result.begin + size;
+
+	return result;
+}
+
+void arena_free(struct arena *arena)
+{
+	if (!arena)
+		return;
+	if (!arena->base)
+		return;
+
+	VirtualFree(arena->base, 0, MEM_RELEASE);
+	arena->base = NULL;
+	arena->begin = NULL;
+	arena->end = NULL;
+}
+
+uint8_t *arena_alloc(struct arena *arena, 
+		ptrdiff_t size, 
+		ptrdiff_t align,
+		ptrdiff_t count)
+{
+	ptrdiff_t padding = -(uintptr_t)arena->begin & (align - 1);
+	ptrdiff_t available = arena->end - arena->begin - padding;
+
+	if (available < 0 || count > available / size)
+		fatal_errorW(L"Out of memory", STATUS_NO_MEMORY);
+
+	uint8_t *p = arena->begin + padding;
+	ptrdiff_t total = count * size;
+	arena->begin += total + padding;
+
+	for (ptrdiff_t i = 0; i < total; i++)
+		p[i] = 0;
+	return p;
+}
+
+
+
---
+++ src/postinst/arena.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <stddef.h>
+#include <stdint.h>
+
+struct arena {
+	uint8_t *base;
+	uint8_t *begin;
+	uint8_t *end;
+};
+
+struct arena arena_create(ptrdiff_t size);
+void arena_free(struct arena *arena);
+
+uint8_t *arena_alloc(struct arena *arena, 
+		ptrdiff_t size, 
+		ptrdiff_t align, 
+		ptrdiff_t count);
+
+#define arena_new(...) arena_newx(__VA_ARGS__, arena_new3, arena_new2)(__VA_ARGS__)
+#define arena_newx(a, b, c, d, ...) d
+#define arena_new2(a, t) (t *)arena_alloc(a, sizeof(t), _Alignof(t), 1)
+#define arena_new3(a, t, n) (t *)arena_alloc(a, sizeof(t), _Alignof(t), n)
