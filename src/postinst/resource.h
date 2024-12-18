c37adf4e66192af85e9a7455e84d1e2293ec18c1
Danila Kondratenko <dan.kondratenko2013@ya.ru>
2024-12-18 15:39:20 +0000

    Add resources

---
+++ src/postinst/resource.h
@@ -0,0 +1,19 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ Compatible
+// Этот файл был автоматически создан RisohEditor 5.8.5.
+// resource.rc
+
+#define IDC_PROGLIST                        100
+#define IDC_PROGRESS                        101
+#define IDC_INSTALLING                      102
+#define IDC_COMMANDS                        103
+
+#ifdef APSTUDIO_INVOKED
+    #ifndef APSTUDIO_READONLY_SYMBOLS
+        #define _APS_NO_MFC                 1
+        #define _APS_NEXT_RESOURCE_VALUE    100
+        #define _APS_NEXT_COMMAND_VALUE     100
+        #define _APS_NEXT_CONTROL_VALUE     1000
+        #define _APS_NEXT_SYMED_VALUE       300
+    #endif
+#endif
