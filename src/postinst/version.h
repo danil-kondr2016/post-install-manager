a6746f83e97f08a4ea881c00a9810b305d8099f4
Danila Kondratenko <dan.kondratenko2013@ya.ru>
2024-12-20 14:51:23 +0000

    Add English translation

--- src/postinst/version.h
+++ src/postinst/version.h
@@ -3,8 +3,18 @@
 #define POSTINST_VERSION_MAJOR    0
 #define POSTINST_VERSION_MINOR    2
 #define POSTINST_VERSION_BUILD    57
-#define POSTINST_VERSION_REV      0
+#define POSTINST_VERSION_REV      1
 
 #ifndef RESOURCE_RC
 extern const char postinst_version[];
+#endif
+#ifdef RESOURCE_RC
+#define _STR(x) #x
+#define STR(x) _STR(x)
+
+#define postinst_version \
+	STR(POSTINST_VERSION_MAJOR) "." \
+	STR(POSTINST_VERSION_MINOR) "." \
+	STR(POSTINST_VERSION_BUILD) "." \
+	STR(POSTINST_VERSION_REV)
 #endif
