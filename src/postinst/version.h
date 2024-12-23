0a3d3fcc907cb036d55a679d2b0be83335a39abc
Danila Kondratenko <dan.kondratenko2013@ya.ru>
2024-12-23 15:41:13 +0000

    Bump version to v0.3.66.0

--- src/postinst/version.h
+++ src/postinst/version.h
@@ -1,9 +1,9 @@
 #pragma once
 
 #define POSTINST_VERSION_MAJOR    0
-#define POSTINST_VERSION_MINOR    2
-#define POSTINST_VERSION_BUILD    57
-#define POSTINST_VERSION_REV      2
+#define POSTINST_VERSION_MINOR    3
+#define POSTINST_VERSION_BUILD    66
+#define POSTINST_VERSION_REV      0
 
 #ifndef RESOURCE_RC
 extern const char postinst_version[];
